fdf1d2b1204419185a677d42e7a5a7eed1f2d259
Dmitry Chornyi <dima.chornyi@gmail.com>
2014-02-03 22:07:21 -0800

    Automatically update the transaction grid

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,14 +7,16 @@ var controllerModule = angular.module('myApp.controllers', []);
 controllerModule.controller('LedgerController', function ($scope, rippleService) {
 
     function init() {
-        $scope.transactions = [];
+        $scope.transactions = new kendo.data.DataSource({
+            "pageSize":10
+        });
         $scope.ledger = null;
     }
 
     init();
 
     rippleService.onTransactionUpdate(function (data) {
-        $scope.transactions.push(data);
+        $scope.transactions.add(data);
     });
 
     rippleService.onLedgerUpdate(function (data) {
